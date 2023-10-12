c76b89bc8b4be72c4057905214d01492965c539b
Nifesimi <oluwanifesimiamao@gmail.com>
2023-10-11 17:11:38 -0700

    sigh

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print all the alphabets in lowercase
+ */
+
+void print_alphabet(void)
+{
+	char letter;
+	for (letter = 'a'; <= 'z'; letter ++)
+		_putchar(letter);
+	_putchar('\n');
+}
