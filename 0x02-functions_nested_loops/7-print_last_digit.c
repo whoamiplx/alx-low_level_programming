d0f2c10f6ccd4ec58a28126e2afd6544cce97dcf
Nifesimi <oluwanifesimiamao@gmail.com>
2023-10-11 18:59:14 -0700

    pld.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_last_digit - prints the last digit of a number
+ * @n: integer to extract last digit from
+ * Return: value of last digit
+ */
+int print_last_digit(int n)
+{
+	int a;
+	if (n < 0)
+	n = -n;
+
+	a = n % 10;
+	if (a < 0 )
+		a = -a;
+	_putchar (a + '0');
+
+	return (a);
+}
