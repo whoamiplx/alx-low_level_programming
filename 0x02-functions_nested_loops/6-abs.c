a8e3b23ad5ed4a97dede43297e4b44b598483c9d
Nifesimi <oluwanifesimiamao@gmail.com>
2023-10-11 18:36:42 -0700

    tired

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - computes the absolute value of an integer
+ * @n: the integer ro be checked
+ * Return: Absolute value of integer
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
+}
