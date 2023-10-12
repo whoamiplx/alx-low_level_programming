8452dd835b42f04f0770aabe18ae2aed8b609b95
Nifesimi <oluwanifesimiamao@gmail.com>
2023-10-11 19:57:12 -0700

    abeg

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add - adds two integers and returns result
+ * @a: integer ti be added to b
+ * @b: integer to be added to a
+ * return: sum of a and b
+ */
+int add(int a , int b)
+{
+	int c;
+	c = a + b;
+	return (c);
+}
