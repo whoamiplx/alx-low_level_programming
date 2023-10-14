464583772c4c5157c9c880a73215354c0d3cb030
Nifesimi <oluwanifesimiamao@gmail.com>
2023-10-14 01:19:17 -0700

    pmn

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -3,14 +3,19 @@
  * print_most_numbers - prints numbers except 2 and 4
  * return: void
  */
-void print_more_numbers(void)
+void print_most_numbers(void)
 {
-	char c;
+	int c;
 
 	for (c = '0'; c <= '9'; c++)
 	{
-		if (!(C == '2'|| c == '4'))
-			_putchar(c);
+		if (c != 2)
+		{
+			if (c != 4)
+			{
+				_putchar(c);
+			}
+		}
 	}
 	_putchar('\n');
 }
