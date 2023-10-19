431b321a714bcfb5f51105bb30835ad1ba955a2b
Nifesimi <oluwanifesimiamao@gmail.com>
2023-10-19 12:56:18 -0700

    advanced task

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - a c program that prints line to the standard error
- * return 1 (success)
+ * main - Entry point
+ * Return:1
  */
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-	return (1);
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
 }
+
