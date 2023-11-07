bde8600ca65e9d54c16a1d406182660ecdadcce3
Nifesimi <oluwanifesimiamao@gmail.com>
2023-11-07 21:45:28 +0100

    task

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -6,6 +6,7 @@
  * @age: age of dog
  * @owner: owner of dog
  *
+ * description: a struct task of a dog
  */
 struct dog
 {
@@ -13,13 +14,5 @@ struct dog
 	float age;
 	char *owner;
 };
-
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-typedef struct dog dog_t;
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
-char *_strcpy(char *dest,char *src);
-int_strlen(char *s);
 
 #endif
